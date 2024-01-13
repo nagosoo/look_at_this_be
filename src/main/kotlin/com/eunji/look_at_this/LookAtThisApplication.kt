8517b9efbab97b72b4169c8590a3b3c55c2ab020
eunji <nagosoo@kakao.com>
2024-01-13 16:06:55 +0900

    init

---
+++ src/main/kotlin/com/eunji/look_at_this/LookAtThisApplication.kt
@@ -0,0 +1,11 @@
+package com.eunji.look_at_this
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LookAtThisApplication
+
+fun main(args: Array<String>) {
+    runApplication<LookAtThisApplication>(*args)
+}
---
+++ src/test/kotlin/com/eunji/look_at_this/LookAtThisApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.eunji.look_at_this
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LookAtThisApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
